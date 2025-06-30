b7589b8e2870307e60119f66a8137594eb8513cd
Harsini612 <144808905+Harsini612@users.noreply.github.com>
2025-06-30 14:28:51 +0530

    haes 3

--- face-recognition-attendance-system/src/App.jsx
+++ face-recognition-attendance-system/src/App.jsx
@@ -1,35 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import OTPVerification from "./pages/OTPVerification";
-import ResetPassword from "./pages/ResetPassword";
-import Dashboard from "./pages/SampleDashboard";
-import AddEditUser from "./pages/AddEditUser";
-import AdminReportPage from "./pages/AdminReportPage";
-import AttendanceHistory from "./pages/AttendanceHistory";
+import AddEditUser from './pages/AddEditUser';
 import AdminDashboard from './pages/AdminDashboard';
+import AdminReportPage from './pages/AdminReportPage';
 import AdminTeacher from './pages/AdminTeacher';
-
-
+import AttendanceHistory from './pages/AttendanceHistory';
+import LeaveRequests from './pages/LeaveRequests';
+import OTPVerification from './pages/OTPVerification';
+import ResetPassword from './pages/ResetPassword';
+import SampleDashboard from './pages/SampleDashboard';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<AdminDashboard />} /> */}
-        <Route path="/" element={<AdminTeacher />} />
+        <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
         <Route path="/otp" element={<OTPVerification />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-user" element={<AddEditUser />} />
+        <Route path="/sample-dashboard" element={<SampleDashboard />} />
+        <Route path="/add-edit-user" element={<AddEditUser />} />
         <Route path="/admin-report" element={<AdminReportPage />} />
-        <Route path="/atten-hist" element={<AttendanceHistory />} />
-
-
-
+        <Route path="/attendance-history" element={<AttendanceHistory />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
+        <Route path="/teachers" element={<AdminTeacher />} />
+        <Route path="/leave-requests" element={<LeaveRequests />} />
       </Routes>
     </Router>
   );
